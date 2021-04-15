699e6d8621b0b6abe7801a92eb4e63a9dcdf3450
grisem <withoutme1988@gmail.com>
2021-04-15 21:19:04 +0300

    filter 4 is changed(completed)?

--- filter_4.py
+++ filter_4.py
@@ -1,4 +1,7 @@
-
-
+import itertools
+from string import ascii_lowercase
 if __name__ == '__main__':
     print("Test")
+    alphabet = ascii_lowercase
+    for _ in zip(itertools.count(1), alphabet):
+        print(_)
